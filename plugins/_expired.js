9d02f50825eaf115955d5d234e9c151c85f9d3a1
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-16 21:07:10 +0300

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -4,7 +4,7 @@ handler.before = async function (m) {
     let chat = db.data.chats[m.chat]
     if (m.isGroup && chat.groupTime != 0) {
         if (new Date() * 1 >= chat.groupTime) {
-            await this.reply(m.chat, `Waktunya *${this.user.name}* untuk meninggalkan grup, terimakasih`)
+            await this.reply(m.chat, `Zaman *${this.user.name}* gruptan ayrılmak, teşekkür ederim`)
             chat.welcome = false
             this.groupLeave(m.chat)
             chat.groupTime = 0
