14ff892accc0b0beea4dd656fd8517402b6d62c4
EkeLachin <104994662+EkeLachin@users.noreply.github.com>
2022-05-16 20:23:41 +0300

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,7 +1,7 @@
 let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = m => m
 
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|sikerim|ananı|anani|piç|orospu|orospu çocuğu|orospu cocugu|yavşak|yavsak|porno|sex|seks|sikiş|boşalmak|fuck|dick|yarrak|bitch|tits|bastard|asshole/i // kendini ekle
 
 handler.before = function (m, { isOwner, isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return !0
@@ -11,14 +11,14 @@ handler.before = function (m, { isOwner, isBotAdmin }) {
 
     if (!chat.antiBadword && !chat.isBanned && isBadword) {
         user.warning += 1
-        this.send2Button(m.chat, `*Badword terdeteksi!*
+        this.send2Button(m.chat, `*Küfür tespit edildi!!*
 Warning: ${user.warning} / 5
-Jika warning mencapai 5 kamu akan dibanned
+Uyarı 5'e ulaşırsa banlanırsınız/Gruptan atılırsınız!
 
-Type *#on antibadword* untuk menyalakan antibadword
-Type *#astagfirullah* atau *#maaf* untuk mengurangi warning
+antibadword'ü açmak için şu komutu yaz *#on antibadword*
+şunu yaz *#astagfirullah* uyarıyı azaltmak için şunu yaz *#maaf*
 
-“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Nyalakan Antibadword', ',1 antibadword', 'Astaghfirullah', ',maaf', m)
+“YouTube Laçin Eke” (HR. al-Bukhari dan Muslim).`, 'YouTube Laçin Eke', 'Antibadwordü aç', ',1 antibadword', 'Astaghfirullah', ',maaf', m)
         if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
